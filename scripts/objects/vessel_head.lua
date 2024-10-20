b635784a51cc76533aeb77f3300d156a33d5f1a2
SadDiamondMan <139525343+SadDiamondMan@users.noreply.github.com>
2024-10-19 22:35:42 -0400

    outline

--- scripts/objects/vessel_head.lua
+++ scripts/objects/vessel_head.lua
@@ -2,12 +2,12 @@ local vessel_head, super = Class(Object)
 
 function vessel_head:init(vessel, data)
     super:init(self)
-
+    data.head = 2
     self.vessel = vessel
 
     self.outline = Sprite("vii/head/"..data.head.."/outline/walk")
     self.outline:setColor(data.hair_color)
-    --self:addChild(self.outline)
+    self:addChild(self.outline)
 
     self.head_sprite = Sprite("vii/head/"..data.head.."/walk")
     self.head_sprite:setColor(data.skin_color)
@@ -17,8 +17,8 @@ function vessel_head:init(vessel, data)
 
     self.offsets = {
         ["walk/down"] = {0, 0},
-        ["walk/left"] = {-2, -1},
-        ["walk/right"] = {2, -1},
+        ["walk/left"] = {0, -1},
+        ["walk/right"] = {0, -1},
         ["walk/up"] = {0, 0},
         ["battle/idle"] = {0, -2}
     }
@@ -28,14 +28,17 @@ function vessel_head:update_part()
     local spr = self.vessel.sprite_options[1]
     local walk = self.vessel.sprite_options[3]
     local head = self.head_sprite
+    local outline = self.outline
 
     if walk == "walk" then
         local direction = spr:match("^(.*)_") -- Extract the direction part (e.g., "walk/up")
         local frame = tonumber(spr:match("_(%d+)")) -- Extract the frame number
     
         if frame then
+            outline:setSprite(self.head_path.. "outline/" .. direction)
             head:setSprite(self.head_path .. direction) -- Set sprite based on direction
             head.x, head.y = self.offsets[direction][1], 1 - (frame % 2) + self.offsets[direction][2]
+            outline.x, outline.y = self.offsets[direction][1], 1 - (frame % 2) + self.offsets[direction][2]
         end
     else
         local name = spr:match("^(.*)_")
@@ -44,9 +47,11 @@ function vessel_head:update_part()
         if head[name] then
             if head[name] == 1 then
                 head:setSprite(self.head_path .. name)
+                outline:setSprite(self.head_path.. "outline/" .. name)
             else
                 local number = (frame - 1) % head[name] + 1 -- Loop back around if frame exceeds head[name]
                 head:setSprite(self.head_path .. name .. "_" .. number)
+                outline:setSprite(self.head_path.. "outline/" .. name.. "_" .. number)
             end
         else
             local frames = Assets.getFrames(self.head_path .. name)
@@ -59,6 +64,7 @@ function vessel_head:update_part()
 
         local offset = self.offsets[name] or {0, 0}
         head.x, head.y = offset[1], offset[2]
+        outline.x, outline.y = offset[1], offset[2]
     end
     
 end
